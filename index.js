3ef6bb7aca7a9bb9c88a11c26f1d5c8c8de0ff6b
Mahipal Signh <msbatth14@gmail.com>
2022-09-08 14:50:58 -0500

    commit tree e82724884284271396db850616be12d05c53a7a7

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const { template } = require("@babel/core");
+
+function writeCards(names, event) {
+    var tempArray = [];
+    var tempString = "";
+    for(let i = 0; i<names.length; i++){
+        
+        tempString = "Thank you, " + names[i] + ", for the wonderful " + event + " gift!";
+        
+        tempArray.push(tempString);
+    }
+    return tempArray;
+}
+function countDown(starting) {
+
+    while(starting >= 0){
+        console.log(starting);
+        starting--;
+    }
+}
